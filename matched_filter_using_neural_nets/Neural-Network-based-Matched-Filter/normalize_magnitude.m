a9f3827362b0a35f4f015f08863ba813a3496c90
Praveek8051 <praveenk8051@gmail.com>
2020-05-15 10:04:33 +0200

    remove

--- matched_filter_using_neural_nets/Neural-Network-based-Matched-Filter/normalize_magnitude.m
+++
@@ -1,5 +0,0 @@
-function [x,t]=normalize_magnitude(x,f)
-x=x/(max(abs(x)));       %normalize
-O=length(x);
-t=linspace(0,O/f,O);
-
--- matched_filter_using_neural_nets/Neural-Network-based-Matched-Filter/plot_form.m
+++
@@ -1,6 +0,0 @@
-function [frame]= plot_form(y)
-
-[a b]=size(y);
-
-
-frame = y(1:a*b);
